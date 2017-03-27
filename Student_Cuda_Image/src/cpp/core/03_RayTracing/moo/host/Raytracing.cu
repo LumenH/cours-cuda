783bd998586cc57d276a0714898c9bf7e185c55c
SommerNicolas <nicolas.sommer@he-arc.ch>
2017-03-27 10:10:16 +0200

    Add CM et SM to raytracing

--- Student_Cuda_Image/src/cpp/core/03_RayTracing/moo/host/Raytracing.cu
+++ Student_Cuda_Image/src/cpp/core/03_RayTracing/moo/host/Raytracing.cu
@@ -5,11 +5,18 @@
 #include "SphereCreator.h"
 #include "Device.h"
 
+#include "length_cm.h"
+
 using std::cout;
 using std::endl;
 
-extern __global__ void raytracing(uchar4* ptrDevPixels, uint w, uint h, float t, Sphere* ptrDevTabSphere, int nbSphere);
 
+//Global memory part
+extern __global__ void raytracing(uchar4* ptrDevPixels, uint w, uint h, float t, Sphere* ptrDevTabSphere, int nbSphere);
+extern __host__ void uploadGPU(Sphere* tabValue);
+extern __global__ void raytracingGM(uchar4* ptrDevPixels, uint w, uint h, float t, Sphere* ptrDevTabSphere, int nbSphere);
+extern __global__ void raytracingCM(uchar4* ptrDevPixels, uint w, uint h, float t, int nbSphere);
+extern __global__ void raytracingSM(uchar4* ptrDevPixels, uint w, uint h, float t, Sphere* ptrDevTabSphere, int nbSphere);
 Raytracing::Raytracing(const Grid& grid, uint w, uint h, float dt, int nbSphere): Animable_I<uchar4>(grid, w, h, "Raytracing_CUDA"){
     this->dt = dt;
     this->t = 0;
@@ -24,20 +31,30 @@ Raytracing::Raytracing(const Grid& grid, uint w, uint h, float dt, int nbSphere)
     toGM(ptrTabSphere);
 
     //Transfert to CM
-    //toCM(ptrTabSphere);
+    uploadGPU(ptrTabSphere);
 }
 
 void Raytracing::toGM(Sphere* ptrTabSphere){
-    Device::malloc(&ptrDevTabSphere, sizeOctet);
-    Device::memclear(ptrDevTabSphere, sizeOctet);
-    Device::memcpyHToD(ptrDevTabSphere, ptrTabSphere, sizeOctet);
+   Device::malloc(&ptrDevTabSphere, sizeOctet);
+   Device::memclear(ptrDevTabSphere, sizeOctet);
+   Device::memcpyHToD(ptrDevTabSphere, ptrTabSphere, sizeOctet);
 
 }
 
 void Raytracing::process(uchar4* ptrDevPixels, uint w, uint h, const DomaineMath& domaineMath){
+    static int i=0;
     //Device::lastCudaError("Before raytracing");
-    raytracing<<<dg, db>>>(ptrDevPixels, w, h, t, ptrDevTabSphere, nbSphere);
+    if (i%3 == 0){
+	raytracingGM<<<dg, db>>>(ptrDevPixels, w, h, t, ptrDevTabSphere, nbSphere);
+    }
+    else if (i%3 == 1){
+	raytracingCM<<<dg, db>>>(ptrDevPixels, w, h, t, nbSphere);
+    }
+    else if (i%3 == 2){
+	raytracingSM<<<dg, db, sizeOctet>>>(ptrDevPixels, w, h, t, ptrDevTabSphere, nbSphere);
+    }
     //Device::lastCudaError("After raytracing");
+    i++;
 
     //Ajouter barriere synchro implicite
 }
@@ -51,4 +68,5 @@ Raytracing::~Raytracing(){
 void Raytracing::animationStep(){
     t += dt;
 }
+
 
